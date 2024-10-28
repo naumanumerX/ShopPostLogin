9da4ce105fd92e50634b4a1acee4e7cbd6753aee
Nauman <naumanumer10@outlook.com>
2024-10-28 14:26:18 +0000

    Add redux-persist For State Persistence Across Sessions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, miniSerializeError } from "@reduxjs/toolkit";
 import imagesSlice from './slices/addImage.slice'
-const store=configureStore({
-    reducer:{
-        imagesSlice
-    },
-    devTools:true
+import storage from 'redux-persist/lib/storage'
+import {  persistReducer } from "redux-persist";
 
+const config={
+    key:"root",
+    version:1,
+    storage: storage // 
+}
+
+const slices=combineReducers({
+
+    imagesSlice : imagesSlice      // same as imageSlice:imagesSlice but shows error
+})
+
+
+const store=configureStore({
+    reducer:persistReducer(config,slices),
+    devTools:true,
+    middleware:(setup)=>setup({
+        serializableCheck:false
+    })
 })
 
 export default store;
