46911aa4b6e5cc6d0aca5b8222bb530e2317897a
Nauman <naumanumer10@outlook.com>
2024-11-15 23:56:00 +0000

    Add Style To Profile Component

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <>
+    <h1 className= "animate__animated animate__swing">Welcome to Profile</h1>
+    
+    
+    </>
+    
+  )
+}
+
+export default Profile
