215e024d8bd4e853ad137d3416eec7ab405c4ef2
Nauman <naumanumer10@outlook.com>
2024-11-15 23:52:50 +0000

    Add Not Found Folder

---
+++ src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+
+        <div className='flex justify-around items-center min-h-screen'> Not Found || 404 Error </div>
+    </div>
+  )
+}
+
+export default NotFound
